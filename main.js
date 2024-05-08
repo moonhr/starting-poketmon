d021a628b37e5f0ec71c8bd8e74c3bfd5e6d6a8e
moonhr <165625160+moonhr@users.noreply.github.com>
2024-05-08 21:10:45 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,71 +1,60 @@
+
 const root = document.getElementById('root');
+const first = document.getElementById('first');
+const last = document.getElementById('last');
+const poketballs = document.querySelectorAll('#poketballs img[data-poketmon]');
 
-// 태그 제작
-const div = document.createElement('div');
-const h1 = document.createElement('h1');
-const img = document.createElement('img');
-
-
-//구조 제작
-root.append(document.createElement('div'), h1, div, document.createElement('div'));
-root.children[0].append(document.createElement('img'));
-root.children[2].append(document.createElement('div'));
-root.children[2].children[0].append(document.createElement('img'));
-root.children[2].children[0].append(document.createElement('img'));
-root.children[2].children[0].append(document.createElement('img'));
-root.children[3].append(document.createElement('div'), document.createElement('div'), document.createElement('div'));
-root.children[3].id = 'choice';
-root.children[3].children[0].id = 'digimon'
-root.children[3].children[1].id = 'meowth'
-root.children[3].children[2].id = 'docOh'
-
-//컨텐츠
-//* 오박사 시작
-root.children[0].children[0].src = './img/오박사시작.png';
-//* 오박사 멘트
-root.children[1].textContent = '너만의 포켓몬을 골라보렴!';
-//* 포켓볼 이미지 3개
-  for(i=0; i<root.children[2].children[0].children.length; i++){
-    root.children[2].children[0].children[i].src = './img/포켓볼.png'
-}
-
-//결과html
-
-choice.style.display = 'none';
-
-for(i=0; i<3; i++){
-choice.children[i].append(document.createElement('img'), document.createElement('h1'));
-}
-digimon.children[0].src = './img/아구몬.png'
-digimon.children[1].textContent = '아구몬을 골랐구나 세상을 구해보렴'
-
-meowth.children[0].src = './img/냐옹이.png'
-meowth.children[1].textContent = '냐옹이를 골랐구나 세상을 정복해보렴'
-
-docOh.children[0].src = './img/오박사.png'
-docOh.children[1].textContent = '오박사를 골랐구나 대학원에 온걸 환영한다'
-
-
-function open(){
-  if(docOh.style.display == 'none'){
-}
+//* 후에 나타날 요소 display none
+for(let i=0; i<last.children.length; i++){
+  last.children[i].style.display = 'none';
 }
 
 
-//결과페이지 이동
-function move(){
-  let docOh = root.children[0].children[0];
-  let ment = root.children[1];
-  let poketballs = root.children[2].children[0];
-  docOh.style.display = 'none';
-  ment.style.display = 'none';
-  poketballs.style.display = 'none';
-  
-  //나타나게하기
+/**
+ * * display속성 변경 함수
+ * @param {*} target : data속성 일치 확인
+ */
+function appendPoketmon(target){
+  // 포켓몬들을 변수에 할당
+  var poketmons = document.getElementById("last").children;
+  // 포켓몬의 길이만큼 반복
+  for (var i = 0; i < poketmons.length; i++) {
+    // 포켓몬을 하나씩 가져옴
+    var div = poketmons[i];
+    // 포켓몬의 data-poketmon 속성이 target과 같으면
+    if (div.getAttribute('data-poketmon') === target) {
+      // 해당 포켓몬을 보여줌
+      div.style.display = 'flex';
+      div.style.flexDirection = 'column';
+      div.style.alignItems = 'center';
+    } else {
+      // 그렇지 않으면 해당 포켓몬을 숨김
+      div.style.display = 'none';
+    }
+}
 }
 
-
-for (let i = 0; i < root.children[2].children[0].children.length; i++) {
-root.children[2].children[0].children[i].addEventListener('click', move);
+/**
+ * * data속성 일치 시 클릭이벤트 발동
+ */
+// 포켓볼의 길이만큼 반복
+for (var i = 0; i < poketballs.length; i++) {
+  //포켓볼을 클릭하면 함수를 실행
+  poketballs[i].addEventListener('click', function() {
+    //첫번째 화면을 가리고 뒷 화면을 보여줌
+    first.style.display = 'none';
+    last.style.display = 'flex';
+    //변수 타켓에 this(클릭된 이미지 요소)의 data-poketmon 속성을 가져와서 할당
+    let target = this.getAttribute('data-poketmon');
+    //포켓볼을 클릭했을 때 해당 포켓몬을 보여줌
+    appendPoketmon(target);
+  });
 }
-console.log(root.children[2].children[0].children[0]);
+
+//* 모바일환경 시 이미지 사이즈 변경
+if(window.innerWidth < 768){
+  first.children[0].children[0].style.height = '20vh';
+  for(let num=0; num<first.children[2].children.length; num++){
+  first.children[2].children[i].style.height = '15vh';
+  }
+}
